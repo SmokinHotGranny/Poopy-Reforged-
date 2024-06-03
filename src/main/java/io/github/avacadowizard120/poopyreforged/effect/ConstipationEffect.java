61fd327256ba371ececbdf3dc48f2784a7f8dcd2
AvacadoWizard120 <edenjudeurias@gmail.com>
2024-06-03 15:40:22 -0500

    Saving changes before hopping off to update Molten Obsidian Tools

---
+++ src/main/java/io/github/avacadowizard120/poopyreforged/effect/ConstipationEffect.java
@@ -0,0 +1,17 @@
+package io.github.avacadowizard120.poopyreforged.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.NotNull;
+
+public class ConstipationEffect extends MobEffect
+{
+    public ConstipationEffect(MobEffectCategory mobEffectCategory, int color) { super(mobEffectCategory, color); }
+
+    @Override
+    public void applyEffectTick(@NotNull LivingEntity pLivingEntity, int pAmplifier)
+    {
+
+    }
+}
